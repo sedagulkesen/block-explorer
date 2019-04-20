950936ab583ee3fec8309e4e662320dd0752e99e
sedagulkesen <sedagulkesen@gmail.com>
2019-04-20 16:57:18 +0300

    provider changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@
     constructor(props) {
       super(props);
       this.state = { username: "Seda", loggedIn: true, blocks: []};
-      this.web3 = new Web3(Web3.givenProvider || new Web3.providers.WebsocketProvider('ws://localhost:3000'), null, {});
+      this.web3 = new Web3(Web3.givenProvider || new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws')), null, {});
     }
     
     getBlock(blockHash='latest', depth=0){
