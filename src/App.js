bab8c2b30184064a5ba4f541018f351b7c23cd16
sedagulkesen <sedagulkesen@gmail.com>
2019-04-21 02:07:45 +0300

    ui is updated

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@
     constructor(props) {
       super(props);
       this.state = { username: "Seda", loggedIn: true, blocks: []};
+      //this.addOneBlock= this.addOneBlock.bind(this);
       this.web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws'));
-      // this.web3 = new Web3(Web3.givenProvider || new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws')), null, {});
-    }
+        }
     
     getBlock(blockHash='latest', depth=0){
       if(depth < 10)
@@ -19,7 +19,6 @@
         this.web3.eth.getBlock(blockHash).then(r => {
             console.log('response status: ', r)
             obj.state.blocks.push({number: r.number, hash:r.hash, parentHash: r.parentHash });
-            // obj.state.blocks.push(r.parentHash);
             console.log('parent hash :' + r.parentHash);
             this.getBlock(r.parentHash, depth+1)
             console.log(obj.state.blocks);
@@ -30,19 +29,35 @@
         this.setState({blocks:this.state.blocks});
       }
     }
-  
+
+    addOneBlock(block){
+      this.state.blocks.unshift({number:block.number, hash:block.hash, parentHash: block.parentHash});
+      this.state.blocks.pop();
+      console.log("inside add one block "+ this.state.blocks);
+      console.log("inside add one block i am length"+ this.state.blocks.length);
+      //console.log(this.state.blocks
+      this.setState({blocks:this.state.blocks});
+      console.log("inside add one block i am length 2   "+ this.state.blocks.length);
+    }
+
     componentDidMount() {
+      let obj=this
       this.getBlock();
       const subscription = this.web3.eth.subscribe('newBlockHeaders', function(error, result){
         if (!error) {
             console.log(result);
-    
             return;
         }
         console.error(error);
         })
         .on("data", function(blockHeader){
             console.log(blockHeader);
+            obj.addOneBlock(blockHeader);
+            //obj.getBlock(blockHeader.hash);
+            // obj.state.blocks.push({number:blockHeader.number, hash:blockHeader.hash, parentHash: blockHeader.parentHash});
+            // console.log("burda basss " + obj.state.blocks.length);
+            // obj.setState({blocks:obj.state.blocks});
+
         })
         .on("error", console.error);
     
--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -10,25 +10,18 @@ class Block extends Component {
   render() {
     const {number,hash,parentHash,difficulty,gasLimit,gasUsed} = this.props.block;
     return (
-      <div style= {divStyle} >
+    <div style= {divStyle}>
         <div>Block id: {number}</div>
-        {/* <Popup trigger={<button>Details</button>} position="right center">
-      <div>Block hash: {hash}</div>
-      <div>Block parent hash: {parentHash}</div>
-    </Popup> */}
-
-    <Popup style= {popupStyle} trigger={open => (
-      <button style= {buttonStyle} className="button">Details {open ? '' : ''}</button>
-    )}
-    position="right center"
-    closeOnDocumentClick>
-    <div>Block hash: {hash}</div>
-      <div>Block parent hash: {parentHash}</div>
-      <div>Block difficulty: {difficulty}</div>
-      <div>Gas limit: {gasLimit}</div>
-      <div>Gas used: {gasUsed}</div>
+        <Popup style= {popupStyle} trigger={open => (
+            <button style= {buttonStyle} className="button">Details {open ? '' : ''}</button>
+        )} position="right center" closeOnDocumentClick>
+        <div>Block hash: {hash}</div>
+        <div>Block parent hash: {parentHash}</div>
+        <div>Block difficulty: {difficulty}</div>
+        <div>Gas limit: {gasLimit}</div>
+        <div>Gas used: {gasUsed}</div>
       </Popup>
-      </div>
+    </div>
     );
   }
 }
@@ -44,15 +37,17 @@ const divStyle = {
 };
 
 const buttonStyle = {
-    background: "pink",
-    margin: "30px",
-    width:"60px",
-    height:"40px",
-    // padding: "30px",
-    border: '5px coral'
-  };
+     background: "pink",
+     margin: "30px",
+     alignSelf: 'stretch',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+};
 
-  const popupStyle = {
+const popupStyle = {
     width:"400px",
     height:"500px",
-  };
+};
