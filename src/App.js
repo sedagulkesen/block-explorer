48fbac621d666867df577d47fec59bc97a189ca4
sedagulkesen <sedagulkesen@gmail.com>
2019-04-20 19:29:06 +0300

    some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
   import React, { Component,  } from 'react';
-  import logo from './logo.svg';
   import './App.css';
   import Web3 from 'web3';
   import Block from "./components/Block";
@@ -19,7 +18,7 @@
         let obj = this
         this.web3.eth.getBlock(blockHash).then(r => {
             console.log('response status: ', r)
-            obj.state.blocks.push({number: r.number, hash:r.hash, parentHash: r.parentHash });
+            obj.state.blocks.push({number: r.number, hash:r.hash, parentHash: r.parentHash , difficulty: r.difficulty, gasLimit: r.gasLimit, gasUsed: r.gasUSed});
             console.log('parent hash :' + r.parentHash);
             this.setState({blocks:obj.state.blocks});
             this.getBlock(r.parentHash, depth+1)
@@ -35,9 +34,8 @@
     render() {
       return (
         <div className="App">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to {this.state.username} APP</h1>
-            <h2 > Latest Blocks</h2>
+            <h1 className="App-title">Welcome to {this.state.username}'s Block Explorer</h1>
+            <div>     <h2 > Latest Blocks</h2></div>
           <div id= "block" style={{ display: "block" }}>
             {this.state.blocks.map(block => (
               <Block block={block} />
--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
+import Popup from "reactjs-popup";
 
 class Block extends Component {
+
+    handleClick() {
+        console.log('this is button you clicked:', this);
+    }
+
   render() {
-    const {number,hash,parentHash} = this.props.block;
+    const {number,hash,parentHash,difficulty,gasLimit,gasUsed} = this.props.block;
     return (
       <div style= {divStyle} >
-        <div>Block id:{number}</div>
-        <div>Block hash: {hash}</div>
-        <div>My parent hash : {parentHash}</div>
+        <div>Block id: {number}</div>
+        {/* <Popup trigger={<button>Details</button>} position="right center">
+      <div>Block hash: {hash}</div>
+      <div>Block parent hash: {parentHash}</div>
+    </Popup> */}
+
+    <Popup style= {popupStyle} trigger={open => (
+      <button style= {buttonStyle} className="button">Details {open ? '' : ''}</button>
+    )}
+    position="right center"
+    closeOnDocumentClick>
+    <div>Block hash: {hash}</div>
+      <div>Block parent hash: {parentHash}</div>
+      <div>Block difficulty: {difficulty}</div>
+      <div>Gas limit: {gasLimit}</div>
+      <div>Gas used: {gasUsed}</div>
+      </Popup>
       </div>
     );
   }
@@ -18,5 +38,22 @@ export default Block;
 const divStyle = {
   background: "#7FD2F0",
   margin: "30px",
-  padding: "30px"
+  width:"300px",
+  padding: "30px",
+  border: '5px solid pink'
 };
+
+const buttonStyle = {
+    background: "pink",
+    margin: "30px",
+    width:"60px",
+    height:"40px",
+    // padding: "30px",
+    border: '5px coral'
+  };
+
+  const popupStyle = {
+    margin: "30px",
+    width:"400px",
+    height:"500px",
+  };
