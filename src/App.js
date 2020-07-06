3124bb5cc89efc227c43aa31e7809b65907cd8e8
Seda <sedagulkesen@gmail.com>
2020-07-06 18:44:25 +0200

    adds project id to websocket api url

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
-import "./App.css";
-import Web3 from "web3";
-import Block from "./components/Block";
-import Popup from "reactjs-popup";
+import React, { Component } from 'react';
+import './App.css';
+import Web3 from 'web3';
+import Block from './components/Block';
+import Popup from 'reactjs-popup';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: "Seda", loggedIn: true, blocks: [], block: {} };
+    this.state = { username: 'Seda', loggedIn: true, blocks: [], block: {} };
     this.web3 = new Web3(
-      new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws")
+      new Web3.providers.WebsocketProvider(
+        'wss://mainnet.infura.io/ws/v3/d6ba6869d2e74050afbd524c6660502a'
+      )
     );
     this.handlePopup = this.handlePopup.bind(this);
     this.popupButton = React.createRef();
     this.subscription = null;
   }
-  handlePopup = block => {
+  handlePopup = (block) => {
     this.setState({ block: block });
     this.popupButton.current.click();
   };
 
-  async getBlocks(blockHash = "latest", depth = 0) {
+  async getBlocks(blockHash = 'latest', depth = 0) {
     let blocks = [];
     if (depth < 10) {
       const block = await this.web3.eth.getBlock(blockHash);
@@ -30,7 +32,7 @@ class App extends Component {
     return blocks;
   }
 
-  addOneBlock = block => {
+  addOneBlock = (block) => {
     const { blocks } = this.state;
     this.setState({ blocks: [block, ...blocks.slice(0, blocks.length - 1)] });
   };
@@ -39,17 +41,17 @@ class App extends Component {
     const blocks = await this.getBlocks();
     this.setState({ blocks });
     this.subscription = this.web3.eth
-      .subscribe("newBlockHeaders", (error, result) => {
+      .subscribe('newBlockHeaders', (error, result) => {
         if (!error) {
           console.log(result);
           return;
         }
         console.error(error);
       })
-      .on("data", blockHeader => {
+      .on('data', (blockHeader) => {
         this.addOneBlock(blockHeader);
       })
-      .on("error", console.error);
+      .on('error', console.error);
   }
 
   componentWillUnmount = () => {
@@ -57,7 +59,7 @@ class App extends Component {
     this.subscription.unsubscribe((error, success) => {
       if (error) return console.error(error);
       if (success) {
-        console.log("Successfully unsubscribed!");
+        console.log('Successfully unsubscribed!');
       }
     });
   };
@@ -69,10 +71,10 @@ class App extends Component {
           Welcome to {this.state.username}'s Block Explorer
         </h1>
         <div>
-          {" "}
+          {' '}
           <h2> Latest Blocks</h2>
         </div>
-        <div align="center" id="block" style={{ display: "block" }}>
+        <div align="center" id="block" style={{ display: 'block' }}>
           {this.state.blocks.map((block, index) => (
             <Block key={index} block={block} handlePopup={this.handlePopup} />
           ))}
@@ -80,20 +82,19 @@ class App extends Component {
         <Popup
           trigger={
             <button style={invisibleButton} ref={this.popupButton}>
-              {" "}
+              {' '}
             </button>
           }
-          modal
-        >
-          {close => (
+          modal>
+          {(close) => (
             <div className="modal">
               <div className="header">
-                {" "}
-                <b> Block #{this.state.block.number} </b>{" "}
+                {' '}
+                <b> Block #{this.state.block.number} </b>{' '}
               </div>
               <br />
               <div className="content">
-                {" "}
+                {' '}
                 <div>
                   <b>Block hash:</b> {this.state.block.hash}
                 </div>
@@ -120,8 +121,7 @@ class App extends Component {
                   className="button"
                   onClick={() => {
                     close();
-                  }}
-                >
+                  }}>
                   Close
                 </button>
               </div>
@@ -136,16 +136,16 @@ class App extends Component {
 export default App;
 
 const buttonStyle = {
-  background: "white",
-  margin: "10px",
-  alignSelf: "stretch",
+  background: 'white',
+  margin: '10px',
+  alignSelf: 'stretch',
   borderRadius: 5,
   borderWidth: 1,
-  borderColor: "#007aff",
+  borderColor: '#007aff',
   marginLeft: 5,
-  marginRight: 5
+  marginRight: 5,
 };
 
 const invisibleButton = {
-  display: "none"
+  display: 'none',
 };
